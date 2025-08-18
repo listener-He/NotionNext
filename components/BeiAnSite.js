3970f656f8c9c70fbede400cd1dc63940b7e629e
hehh <lingtinzhe@icloud.com>
2025-08-18 17:47:50 +0800

    feat(BeiAnSite): 添加茶ICP备案信息并更新萌ICP备案图标
    更新萌ICP备案的图标样式并添加新的茶ICP备案信息，以符合最新的备案要求

--- components/BeiAnSite.js
+++ components/BeiAnSite.js
@@ -16,9 +16,11 @@ export default function BeiAnSite() {
       <a href={beianLink} className='mx-1'>
         {beian}
       </a> &nbsp;
-      <i className='fas fa-shield-alt' />
-      <a href="https://icp.gov.moe/?keyword=20257012" target="_blank">萌ICP备20257012号</a>
+      <img style="width:32px;height:32px;margin-bottom:-8px" src="https://icp.gov.moe//images/ico64.png" alt="萌ICP备案图标"/>
+      <a href="https://icp.gov.moe/?keyword=20257012">萌ICP备20257012号</a>
       <br />
+      <img src="https://icp.redcha.cn/static/picture/icplogoi.png" style="height: 20px;" alt="茶ICP备案图标"/>
+      <a href="https://icp.redcha.cn/beian/ICP-2025080099.html">茶ICP备2025080099号</a>
     </span>
   )
 }
