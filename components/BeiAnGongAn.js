e5e45851891a5a1b123b35e7b42b682aaf19739d
hehh <lingtinzhe@icloud.com>
2025-08-26 23:12:13 +0800

    feat(components): 添加茶ICP备案和晓梦羊备案
    - 在 BeiAnGongAn 组件中添加茶ICP备案图标和链接
    - 添加晓梦羊备案号和链接，使用自定义样式- 在 Footer 组件中调整版权年份显示逻辑

--- components/BeiAnGongAn.js
+++ components/BeiAnGongAn.js
@@ -24,6 +24,42 @@ export const BeiAnGongAn = props => {
       </a> &nbsp;
       <LazyImage src='https://blog-file.hehouhui.cn/20250724094940374.png' width={15} height={15} alt='萌ICP备20257012号'/>
       <a href="https://icp.gov.moe/?keyword=20257012" target="_blank">萌ICP备20257012号</a>
+      &nbsp;
+      <LazyImage src="https://icp.redcha.cn/static/picture/icplogoi.png" width={20} height={20} alt="茶ICP备案图标"/>
+      <a href="https://icp.redcha.cn/beian/ICP-2025080099.html">茶ICP备2025080099号</a>
+       &nbsp;
+      <a href="https://bloginc.cn/18872.html">
+        <div style={{
+          display: 'inline-flex',
+          height: '24px',
+          fontFamily: 'Arial, sans-serif',
+          fontSize: '14px',
+          lineHeight: '24px',
+          textAlign: 'center',
+          borderRadius: '8px',
+          overflow: 'hidden',
+          boxShadow: '0 2px 5px rgba(0,0,0,0.1)',
+        }}>
+          <div style={{
+            backgroundColor: '#04314D',
+            color: 'white',
+            padding: '0 12px',
+            borderTopLeftRadius: '8px',
+            borderBottomLeftRadius: '8px',
+          }}>
+            晓梦羊&reg;
+          </div>
+          <div style={{
+            backgroundColor: '#FF6A00',
+            color: 'white',
+            padding: '0 12px',
+            borderTopRightRadius: '8px',
+            borderBottomRightRadius: '8px',
+          }}>
+            18872号
+          </div>
+        </div>
+      </a>
     </div>
   )
 }
