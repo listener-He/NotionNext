6cc1dc8efee1b50e23a4e371a67f03fbd1a788e8
hehh <lingtinzhe@icloud.com>
2026-01-08 23:11:33 +0800

    feat(components): 更新PoweredBy组件添加链接和Zeabur支持
    - 将Vercel和Netlify文本转换为可点击链接
    - 添加Zeabur链接支持
    - 为所有链接添加新窗口打开功能
    - 保持原有的样式和布局结构

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -14,7 +14,7 @@ export default function PoweredBy(props) {
         NotionNext {siteConfig('VERSION')}
       </a>
       &nbsp;
-      <span className='mr-1 text-gray-500 dark:text-indigo-300'>Vercel & Netlify</span>
+      <span className='mr-1 text-gray-500 dark:text-indigo-300'><a href='https://vercel.com/' target="_blank" >Vercel</a> <a href='https://www.netlify.com/' target='_blank'>Netlify</a> <a href='https://zeabur.cn/' target='_blank'>Zeabur</a></span>
     </div>
   )
 }
